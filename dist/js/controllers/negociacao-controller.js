ef77f7a12c81fbcbd36a2cebf5d0adb6bb20cc44
Misael David <misadavi11@gmail.com>
2023-05-11 16:52:31 -0300

    first commit

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,39 @@
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+import { MensagemView } from "../views/mensagem-view.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+export class NegociacaoController {
+    constructor() {
+        this.negociacoes = new Negociacoes();
+        this.negociacoesView = new NegociacoesView('#negociacoesView', true);
+        this.mensagemView = new MensagemView('#mensagemView');
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.uptade(this.negociacoes);
+    }
+    adiciona() {
+        const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
+        if (!this.ehDiaUtil(negociacao.data)) {
+            this.mensagemView.uptade('Apenas negociações em dias úteis são aceitas!');
+            return;
+        }
+        this.negociacoes.adiciona(negociacao);
+        this.limparFormulario();
+        this.atualizaView();
+    }
+    ehDiaUtil(data) {
+        return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+    atualizaView() {
+        this.negociacoesView.uptade(this.negociacoes);
+        this.mensagemView.uptade('Negociação adicionada com sucesso!');
+    }
+}
